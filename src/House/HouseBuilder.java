aa2cacce86a3f9338539c71e7bde022a253da9bb
karol <karol.malysa@interia.pl>
2021-03-10 11:08:07 +0100

    start

---
+++ src/House/HouseBuilder.java
@@ -0,0 +1,4 @@
+package House;
+
+public interface HouseBuilder {
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import House.House;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        House house1 = new House("wall1", "floor1", "room1", "window1","door1","garage");
+//        House house2 = new House();  // - założenie: mam dużo konstruktorów i nie jestem pewny co do kolejności pól
+//                                                     i troche zaczynam się w tym gubić
+
+
+
+    }
+}
